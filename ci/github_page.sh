009f37b8ef60f6f3a55aa43406603d653c23911d
Jichao Ouyang <oyanglulu@gmail.com>
2015-11-22 21:06:07 +0800

    circle ci

---
+++ ci/github_page.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x -e
+cd examples/todomvc
+npm run build
+cd ../..
+mkdir -p public/todomvc
+cp -r examples/todomvc/public public/todomvc
+cd public
+git checkout -b gh-pages
+git config --global user.email "oyanglulu@gmail.com"
+git config --global user.name $CIRCLE_PROJECT_USERNAME
+git add .
+git commit -m "publi:ship: $CIRCLE_BUILD_NUM"
+git push origin gh-pages
